ab29b1153783b3dace669a6c145b734635bbc2a4
Giorgiocirrone <cirronegiorgio@gmail.com>
2024-08-08 21:39:29 +0200

    commit senza boot

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-const target = document.getElementById('target');
-
-const list = document.createElement('div');
-
-for (g = 1; g <= 10; g++) {
-
-    const listItem = document.createElement('li');
-
-    listItem.append(`numero${g}`);
-
-    list.appendChild(listItem);
-
-    target.appendChild(list);
+console.log('JS OK');
 
 
+const resultElement = document.getElementById('fizz-buzz');
 
+const multipleOf3 = 'Fizz';
+const multipleOf5 = 'Buzz';
 
+let list = '';
 
+for (i = 1; i <= 100; i++) {
+    const li = document.createElement('li');
+
+    if (i % 3 === 0) {
+        li.append(multipleOf3);
+        if (i % 5 === 0) {
+            li.append(multipleOf5);
+            li.classList.add('bg-fizz-buzz');
+        }
+        else {
+            li.classList.add('bg-fizz');
+        }
+    }
+    else if (i % 5 === 0) {
+        li.append(multipleOf5);
+        li.classList.add('bg-buzz');
+    }
+    else {
+        li.append(i);
+        li.classList.add('bg-number');
+    }
+
+    resultElement.appendChild(li);
 }
